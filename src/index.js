3a2f86040268d72247bf9a9a3fa30ec536fa03b0
José Luis de Figueiredo <jldefigueiredo@gmail.com>
2021-11-26 10:52:22 -0300

    Actualizado con objetos literales

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
+
+////////////// objetos literales
+
+const object1 = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+}
+
+const object2 = {
+  name: 'Full Stack web application development',
+  level: 'intermediate studies',
+  size: 5,
+}
+
+const object3 = {
+  name: {
+    first: 'Dan',
+    last: 'Abramov',
+  },
+  grades: [2, 3, 5, 3],
+  department: 'Stanford University',
+}
+
+/////////////////////////
 const Header = (course) => {
   return (
     <div>
